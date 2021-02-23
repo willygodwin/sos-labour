1e257c3207f2e257c22683e7365d1004be983c2c
willy <willygodwin47@gmail.com>
2021-02-23 20:28:29 +0800

    Added config, index.js etc.

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
-
+require("dotenv").config();
+const db = require('./models');
+// same as
+// const db = require('./models/index');
 const app = express();
 
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log("App is running on http://localhost:" + PORT)
-});
+db.sequelize
+    .sync()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log("App is running on http://localhost:" + PORT)
+        });
+    });
+
+
 
