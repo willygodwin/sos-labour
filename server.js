21562045516d243f787a9d43f8d9f7db8a19b38d
willy <willygodwin47@gmail.com>
2021-02-23 19:52:04 +0800

    setup boioler plate

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-function name(){
-    await fetch();
-}
+const express = require('express');
+
+const app = express();
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log("App is running on http://localhost:" + PORT)
+});
+
