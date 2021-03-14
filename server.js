10feb7afff5f9b2d67ac3bdcdc53029b4ff00c5c
willy <willygodwin47@gmail.com>
2021-03-14 16:22:10 +0800

    added images to file

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const app = express();
 const db = require(path.join(__dirname,'models'));
 const router = require(path.join(__dirname,'routes','routes.js'));
 // Creating express app and configuring middleware needed for authentication
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(express.static(path.join(__dirname,'public')));
 
 // We need to use sessions to keep track of our user's login status
